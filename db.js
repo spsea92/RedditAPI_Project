5b25d3aeb25f469d2e89557be4405fc72ae2be0f
unknown <airandfingers@gmail.com>
2019-08-01 06:55:47 +0100

    Add commented-out Mongo connection code

--- db.js
+++ db.js
@@ -3,6 +3,16 @@ mongoose.Promise = Promise
 
 const connectPromise = mongoose.connect('mongodb://post-collector:iloveredditposts1@ds157857.mlab.com:57857/reddit-posts')
 
+/* Should be:
+const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env
+
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`)
+
+To tell if this will work, run this before "node script0":
+  $ echo $MONGO_USER
+  post-collector
+*/
+
 const postsSchema = mongoose.Schema({
   id: String, // the Reddit ID of the post
   title: String, // the post title
